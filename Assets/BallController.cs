c73511009bbaa28585ce6221de6ad8440bf99414
Robert Lafferty <rclafferty@k-state.edu>
2018-10-01 01:49:36 -0500

    Working Version of Demo From Meeting

---
+++ Assets/BallController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    float velocity = 0.10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 position = transform.position; //gets position
+        position.x = position.x + velocity; //edits position
+        //set new position
+        transform.position = position;
+    }
+
+    //action taken on collision
+    private void OnCollisionEnter(Collision collision)
+    {
+        velocity *= -1;
+        
+    }
+
+}
