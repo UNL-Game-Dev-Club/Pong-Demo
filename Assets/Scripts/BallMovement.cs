3997a731f898126de21b38a405e516ce6f334596
Robert Lafferty <rclafferty@ksu.edu>
2018-12-11 21:42:35 -0600

    Updated With Wiki Walkthrough Code

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+
+    [SerializeField]
+    float speed;
+    float verticalStep;
+
+    // Use this for initialization
+    void Start () {
+        speed = 5.0f;
+        verticalStep = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float distanceTraveled = speed * Time.deltaTime;
+        float verticalTraveled = verticalStep * Time.deltaTime;
+
+        Vector2 translationVector = new Vector2();
+        translationVector.x = distanceTraveled;
+        translationVector.y = verticalTraveled;
+        this.transform.Translate(translationVector);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name.Contains("Paddle"))
+        {
+            ChangeHorizontalDirection();
+        }
+        else if (collision.collider.name.Contains("Wall"))
+        {
+            ChangeVerticalStepDirection();
+        }
+    }
+
+    void ChangeHorizontalDirection()
+    {
+        speed = -1 * speed;
+        verticalStep = Random.value;
+    }
+    
+    void ChangeVerticalStepDirection()
+    {
+        verticalStep = -1 * verticalStep;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    float deadValue;
+    float speed;
+    
+    [SerializeField]
+    float verticalInput;
+
+    Rigidbody thisRigidbody;
+
+	// Use this for initialization
+	void Start () {
+        deadValue = 0.5f;
+        speed = 3.0f;
+        verticalInput = 0.0f;
+
+        thisRigidbody = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        verticalInput = Input.GetAxis("Vertical");
+
+        if (verticalInput > deadValue)
+        {
+            // Move Up
+            Vector2 translationVector = new Vector2();
+            translationVector.x = 0.0f;
+            translationVector.y = speed * Time.deltaTime;
+            this.transform.Translate(translationVector);
+
+        }
+        else if (verticalInput < 0 - deadValue)
+        {
+            // Move Down
+            Vector2 translationVector = new Vector2();
+            translationVector.x = 0.0f;
+            translationVector.y = -1 * speed * Time.deltaTime;
+            this.transform.Translate(translationVector);
+        }
+        else
+        {
+            // Stay still
+            thisRigidbody.velocity = Vector3.zero;
+            thisRigidbody.angularVelocity = Vector3.zero;
+        }
+    }
+}
